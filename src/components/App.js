2e2674cd9cc9064e7e1e8b30394fd28f5330dd0a
NikoTiN161 <dmitrii.asenenko@gmail.com>
2024-01-15 22:32:03 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <h1>Helo</h1> 
+    </>
+  );
+}
+
+export default App;
