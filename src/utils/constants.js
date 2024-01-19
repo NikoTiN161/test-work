ee2210690220dd3282d164610c5d880db522a24f
Dmitriy Aksenenko <dmitrii.asenenko@gmail.com>
2024-01-19 11:23:58 +0300

    edit https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const optionsDadata = {
-    url: 'http://suggestions.dadata.ru/suggestions/api/4_1/rs',
+    url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs',
     headers: {
         'Content-Type': 'application/json',
         "Accept": "application/json",
